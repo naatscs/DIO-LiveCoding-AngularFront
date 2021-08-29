c5c13bcbffbdcc38640d3caadc4a52d0aab4ea1e
naatscs <naatscs@gmail.com>
2021-08-29 18:05:22 -0300

    ajusteapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { BookCartComponent } from './components/book-cart/book-cart.component';
+import { ProductListComponent } from './components/book-cart/product-list/product-list.component';
+import { FiltersComponent } from './components/book-cart/filters/filters.component';
+
+import { ProductItemComponent } from './components/book-cart/product-list/product-item/product-item.component';
+import { HttpClientModule } from '@angular/common/http';
+import {BooksService} from './components/book-cart/product-list/product-list.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    BookCartComponent,
+    ProductListComponent,
+    FiltersComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [BooksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
